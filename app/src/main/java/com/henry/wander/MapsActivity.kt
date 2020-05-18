0ca5091249b7eec8dcac7df562fafd843ef68c35
henryfebryan24 <henry24febryan@gmail.com>
2020-05-18 11:26:37 +0700

    add overlay

--- app/src/main/java/com/henry/wander/MapsActivity.kt
+++ app/src/main/java/com/henry/wander/MapsActivity.kt
@@ -11,10 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -46,11 +43,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         //These coordinates represent the latitude and longitude of the Googleplex.
         val latitude = -6.203861
         val longitude = 106.782836
-        val zoomLevel = 20f
+        val zoomLevel = 18f
+        val overlaySize = 100f
 
         val homeLatLng = LatLng(latitude, longitude)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
         map.addMarker(MarkerOptions().position(homeLatLng))
+
+        val androidOverlay = GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.android))
+                .position(homeLatLng, overlaySize)
+
+        map.addGroundOverlay(androidOverlay)
         setMapLongClick(map)
         setPoiClick(map)
         setMapStyle(map)
